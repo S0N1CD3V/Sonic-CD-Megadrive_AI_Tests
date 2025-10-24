35f67c7f84ffca27b9db65ba5d4fbf825738f821
S0N1C <78824905+S0N1CD3V@users.noreply.github.com>
2025-10-25 01:46:03 +0200

    removed redundant dissasembly files.
    removed redundant dissasembly files.

--- Old Dissembly/Dont Use Me and Dont Deletle me.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-asm68k /k /p /o ae- sonic.asm, s1built.bin >errors.txt, sonic.sym, sonic.lst
-convsym sonic.lst s1built.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
-fixheadr.exe s1built.bin
